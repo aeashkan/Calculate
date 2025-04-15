0e753e974ac03ed8552eded387ecc8f794298cc6
aeashkan <160534061+aeashkan@users.noreply.github.com>
2025-04-15 23:19:41 +0330

    API
    Use API and Postman

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const config = require('config');
-const {User} = require('./model/user');
+const _ = require('lodash');
+const { User } = require('./model/user');
 
 console.log(`*** ${String(config.get('Level')).toUpperCase()} ***`);
- 
-const newUser = new User({
-    fullname: 'Ashkan Elahi',
-    email: 'ashkan.elahi99@gmail.com',
-    password: '848413ae'
+
+const app = express();
+
+app.listen(config.get('PORT'), () => {
+    console.log(`Server is running on port ${config.get('PORT')}`);
+});
+app.use(express.json());
+app.post('/api/users', (req, res) => {
+    const body = _.pick(req.body, ['fullname', 'email', 'password']);
+    const user = new User(body);
+    console.log(body);
+    
+
+    user.save().then((user) => {
+        res.status(200).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
-newUser.save().then((user) => {
-    console.log('User created', user);
-    
-})
+
