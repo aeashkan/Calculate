2879c22a9fe580b566ff0ab5d1d9ea5e8380096d
aeashkan <160534061+aeashkan@users.noreply.github.com>
2025-04-19 23:19:42 +0330

    hash
    hash password in database

--- model/user.js
+++ model/user.js
@@ -1,5 +1,7 @@
 const {mongoose} = require('../db/mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
 
 const userSchema = new mongoose.Schema({
     fullname: {
@@ -28,6 +30,25 @@ const userSchema = new mongoose.Schema({
 
 });
 
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'fullname', 'email']);
+}
+
+userSchema.pre('save', function(next)  {
+    const user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 
